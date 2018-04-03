8bf37ff21cd072caed1f819a5080abcd41e71bb7
tahsinrahman <tahsynx@gmail.com>
2018-04-03 16:44:30 +0600

    test

--- hack/concourse/unit_test.sh
+++ hack/concourse/unit_test.sh
@@ -6,5 +6,6 @@ mkdir -p $GOPATH/src/github.com/appscode
 cp -r stash $GOPATH/src/github.com/appscode
 cd $GOPATH/src/github.com/appscode/stash
 
+go get golang.org/x/tools/cmd/goimports
 ./hack/make.py
 ./hack/make.py test unit
