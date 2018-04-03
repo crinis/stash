96c6be1fae6bc6dcf32ea053fc46c1d392ae508c
tahsinrahman <tahsynx@gmail.com>
2018-04-03 15:41:48 +0600

    fix dir

--- hack/concourse/unit_test.sh
+++ hack/concourse/unit_test.sh
@@ -4,6 +4,6 @@ set -x -e
 
 mkdir -p $GOPATH/src/github.com/appscode
 cp -r stash $GOPATH/src/github.com/appscode
-cd $GOPATH/src/github.com/appscode/stash
+cd $GOPATH/src/github.com/appscode/stash/hack
 
 ./make.py test unit
